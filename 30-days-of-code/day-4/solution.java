7b91ab8fc36b26e2c0e908ae984d1c995392ea9e
Doljinsuren <doljko927@gmail.com>
2019-01-25 20:05:17 +0900

    Create solution.java

---
+++ 30-days-of-code/day-4/solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+  
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
