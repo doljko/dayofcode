cca145f32039699c2e915b7da6f299f8f18bc4f6
doljko <e.doljinsuren@chinggis.systems>
2019-10-22 10:17:56 +0800

    users component created and onInit.

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { User } from '../../models/User';
+import { User } from "../../models/User";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
-
-export class UserComponent {
+export class UserComponent implements OnInit {
   // Properties
   user: User;
 
   // Methods
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     this.user = {
-      firstName: 'John',
-      lastName: 'Doe',
+      firstName: "John",
+      lastName: "Doe",
       age: 30,
       address: {
-        street: '50 Main st',
-        city: 'Boston',
-        state: 'MA'
+        street: "50 Main st",
+        city: "Boston",
+        state: "MA"
       }
-    }
-  } 
+    };
+  }
 }
